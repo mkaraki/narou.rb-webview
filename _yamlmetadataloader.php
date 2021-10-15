4838ff08754fe86b08f5b091976015ebe6103baa
mkaraki <masakazu.araki@mkarakiapps.com>
2021-10-15 13:12:02 +0900

    Add error message when load not found story or novel

--- _yamlmetadataloader.php
+++ _yamlmetadataloader.php
@@ -23,6 +23,9 @@ function loadToc($id, $loadedindex = null)
     global $narounoveldir;
     if ($loadedindex === null) $loadedindex = loadIndex();
 
+    if ($id >= count($loadedindex))
+        die('該当の小説が見つかりませんでした。');
+
     $indexdata = $loadedindex[$id];
     $tocpath = $narounoveldir . DIRECTORY_SEPARATOR . $indexdata['sitename'] . DIRECTORY_SEPARATOR . $indexdata['file_title'] . DIRECTORY_SEPARATOR . 'toc.yaml';
     return loadYaml($tocpath);
@@ -33,6 +36,9 @@ function loadContent($storyid, $novelid, $loadedtoc = null, $loadedindex = null)
     global $narounoveldir;
     if ($loadedindex === null) $loadedindex = loadIndex();
     if ($loadedtoc === null) $loadedtoc = loadToc($novelid, $loadedindex);
+
+    if ($storyid >= count($loadedtoc['subtitles']))
+        die('該当の話が見つかりませんでした。');
 
     $novelcontentdir  = $narounoveldir . DIRECTORY_SEPARATOR . $loadedindex[$novelid]['sitename'] . DIRECTORY_SEPARATOR . $loadedindex[$novelid]['file_title'] . DIRECTORY_SEPARATOR . '本文';
     $novelcontentpath = $novelcontentdir . DIRECTORY_SEPARATOR . $loadedtoc['subtitles'][$storyid]['index'] . ' ' . $loadedtoc['subtitles'][$storyid]['file_subtitle'] . '.yaml';
