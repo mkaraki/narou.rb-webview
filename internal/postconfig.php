212926792c4752c0892e0718ad84ff0cfeb96f50
mkaraki <git@su.mkarakiapps.com>
2025-11-05 08:27:15 +0900

    Remove old project

--- internal/postconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../__config.php';
-define('NAROU_DIR', $naroudir);
-define('NAROU_DB_YAML', $naroudir . DIRECTORY_SEPARATOR  . '.narou' . DIRECTORY_SEPARATOR . 'database.yaml');
-define('NAROU_NOVEL_DIR', $naroudir . DIRECTORY_SEPARATOR  . '小説データ');
